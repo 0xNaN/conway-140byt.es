ab9e392758c76de35ec99b70ab4d8be80b534ab4
0xNaN <enzodicicco@gmail.com>
2012-10-24 13:32:35 +0200

    updated

--- conway132.js
+++ conway132.js
@@ -1,1 +1,1 @@
-function(g,x,y,w,h){v=g[x][y];s=0;for(i=x-1;i<x+2;i++)for(k=y-1;k<y+2;k++)if(i>=0&&i<w&&k>=0&&k<h)s+=g[i][k];return(s==3||(v&&s==4))?1:0;}
+function(g,x,y,w,h){v=g[x][y];s=0;for(i=x-1;i<x+2;i++)for(k=y-1;k<y+2;k++)if(i>=0&&i<w&&k>=0&&k<h)s+=g[i][k];return s==3||v&&s==4;}
